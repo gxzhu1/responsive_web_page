aa76144060894a93ecb51baa42c6a41f4b8154f7
gxzhu <gxzhu@uw.edu>
2025-01-28 17:55:32 -0800

    Creating a website

---
+++ js/main.js
@@ -0,0 +1,52 @@
+// Get the element with the class "icon"
+let icon = document.getElementsByClassName("icon")[0];
+
+// Add an event listener for the 'click' event on the icon element
+icon.addEventListener('click', responsive_control);
+
+// Function to control the responsiveness of the navigation bar
+function responsive_control() {
+  // Get the element with the id "myTopnav"
+  let x = document.getElementById("myTopnav");
+
+  // Check if the class name of the element is "topnav"
+  if (x.className === "topnav") {
+    // If it is, add the "responsive" class to the element
+    x.className += " responsive";
+  } else {
+    // If it's not, remove the "responsive" class from the element
+    x.className = "topnav";
+  }
+}
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+function myFunction(imgs) {
+  // Get the expanded image
+  var expandImg = document.getElementById("expandedImg");
+  // Get the image text
+  var imgText = document.getElementById("imgtext");
+  // Use the same src in the expanded image as the image being clicked on from the grid
+  expandImg.src = imgs.src;
+  // Use the value of the alt attribute of the clickable image as text inside the expanded image
+  imgText.innerHTML = imgs.alt;
+  // Show the container element (hidden with CSS)
+  expandImg.parentElement.style.display = "block";
+}
